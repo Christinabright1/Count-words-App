413f2d6ca18d8291173703d59e52306f2117934a
Christine Anani <ananichristine2333@gmail.com>
2019-11-15 19:30:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //select elements
 
+//select elements
+//textarea
+let textContent= document.getElementById("displayText");
 //box inside 
-let displayValue= document.getElementById("displayLength");
-//button to be click to display the value of the length
-let clickButton= document.getElementById("button");
+let length= document.getElementById("displayLength");
+//add input event Listener to display words
 
-clickButton.addEventListener("click", length);
-
-//write lengthofWord function
-
-function length(){
-  let lenghtofwords= document.getElementById("displayText").value.length;
-  displayValue.innerHTML= lenghtofwords;
-}
+textContent.addEventListener("input", function(){
+  //get value
+  let value= textContent.value;
+  //let split value into array and get it length
+  length.innerHTML= value.split(" ").length;
+});
